1536b9c4288efe0ac6f2470860d70e4d3e9b91f8
KrysDebek <krystian.debek.be@gmail.com>
2025-11-02 15:48:24 +0100

    added env to api key in vide stats

--- video_stats.py
+++ video_stats.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
-API_KEY = "key"
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
 
 
@@ -9,15 +13,19 @@ def get_playlist_id():
 
     try:
 
-        url = "https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
         response = requests.get(url)
+
+        response.raise_for_status()
 
         data = response.json()
 
         channel_items = data['items'][0]
 
         channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+
+        print(channel_playlistId)
 
         return channel_playlistId
     
